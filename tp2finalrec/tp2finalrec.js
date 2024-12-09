6f45a04542ce5c309ab041eb5743e9b35f2ebd93
agostinacordomi <129205761+agostinacordomi@users.noreply.github.com>
2024-12-09 19:24:56 -0300

    tp2finalrec.js

--- tp2finalrec/tp2finalrec.js
+++ tp2finalrec/tp2finalrec.js
@@ -1,18 +1,20 @@
 let juego;
-let fondoimg, autoimg, rocaimg;
+let fondoimg, fondo2img, autoimg, rocaimg;
 let pantalla = 0;
 let tiempo;
-//https://youtu.be/N3TrFQP8n_I
-
 var colision;
 let objetos = [];
 let ultimoTiempo = 0;
+let titulo;
+let fuenteTitulo;
 
 function preload() {
   fondoimg = loadImage('data/fondo.png');
+  fondo2img = loadImage('data/fondo2.jpg');
   autoimg = loadImage('data/auto1.png');
   rocaimg = loadImage('data/roca1.png');
-  colision = loadSound('data/colision.wav');
+  colision = loadSound('data/colision.mp3');
+  fuenteTitulo = loadFont('data/ComicalCartoon.ttf');
 }
 
 function setup() {
@@ -21,6 +23,7 @@ function setup() {
   juego.iniciar();
   auto = new Auto();
   tiempo = new Timer(30); 
+  titulo = "La Máscara";
 }
 
 function draw() {
@@ -52,36 +55,53 @@ function draw() {
     }
 
   } else if (pantalla === 2) {
-    mostrarCreditos();
+    creditosPerdiste();
     if (keyIsPressed) {
       if (key === 'R' || key === 'r') {
       pantalla = 0 }
     }
   }
+  
+  else if (pantalla === 3) {
+    creditosGanaste();
+  }
 }
 
 function mostrarInstrucciones() {
+  background(fondo2img);
   textAlign(CENTER, CENTER);
   textSize(20);
   fill(255);
   text(
     "Usa las teclas A y D para mover el auto.\n" +
       "Evita los obstáculos y acumula puntos.\n" +
-      "Presiona cualquier tecla para comenzar.",
+      "Presiona la tabla espaciadora para comenzar",
     width / 2,
-    height / 2
+    height / 2 + 100
   );
+  
+    textSize(15);
+  if (fuenteTitulo) {
+    textFont(fuenteTitulo);  
+  }
+  fill(255);
+  textAlign(CENTER);
+  text("'Coraje el perro cobarde \n y La Mascara' \n El juego", width / 2, height / 2 - 50);
+  
+
+  textFont('Tahoma');
+  textSize(18);
 
   if (keyIsPressed) {
-     if (key === 'S' || key === 's') {
-    tiempo.reiniciar(); // Reinicia el temporizador al iniciar el juego
+     if (key === ' ' || key === ' ') {
+    tiempo.reiniciar(); 
     juego.reiniciar();
-    pantalla = 1; // Cambia a la pantalla del juego
+    pantalla = 1; 
   }
 }
 }
 
-function mostrarCreditos() {
+function creditosPerdiste() {
   textAlign(CENTER, CENTER);
   textSize(15);
   fill(200);
@@ -92,4 +112,35 @@ function mostrarCreditos() {
     width / 2,
     height - 40
   );
+  
+  textAlign(CENTER, CENTER);
+  textSize(20);
+  fill(255);
+  text(
+    "¡PERDISTE!\n" +
+    "PRESIONA 'R' PARA JUGAR DE NUEVO",
+    width / 2,
+    height / 2)
+}
+
+function creditosGanaste() {
+  textAlign(CENTER, CENTER);
+  textSize(15);
+  fill(200);
+  text(
+    "Juego desarrollado por Agostina Cordomi y Nicolás Alberti.\n" +
+      "PMIW Comisión 2.\n" +
+      "Gracias por jugar!",
+    width / 2,
+    height - 40
+  );
+  
+  textAlign(CENTER, CENTER);
+  textSize(20);
+  fill(255);
+  text(
+    "¡GANASTE!\n" +
+    "PRESIONA 'R' PARA JUGAR DE NUEVO",
+    width / 2,
+    height / 2)
 }
